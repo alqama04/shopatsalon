508d1e9b7c20baf3d1159c43e325d05c34deb3c6
alqama04 <alqamaazmi11@gmail.com>
2024-01-13 18:11:57 +0530

    initial commit

--- src/app/dashboard/(dashboard-content)/page.tsx
+++ src/app/dashboard/(dashboard-content)/page.tsx
@@ -4,28 +4,28 @@ import Reward from "./Reward";
 import { Skeleton, Skeleton2 } from "@/components/Skeleton";
 import fetchData from "./fetchData";
 
-const Levels = dynamic(() => import("./(currentCycle)/Levels"), {
-  ssr: false,
-  loading() {
-    return (
-      <div>
-        <Skeleton2 className="bg-gray-800" />
-      </div>
-    );
-  },
-});
+// const Levels = dynamic(() => import("./(currentCycle)/Levels"), {
+//   ssr: false,
+//   loading() {
+//     return (
+//       <div>
+//         <Skeleton2 className="bg-gray-800" />
+//       </div>
+//     );
+//   },
+// });
 
-const CyclePurchase = dynamic(() => import("./(currentCycle)/CyclePurchase"), {
-  loading() {
-    return (
-      <div>
-        <Skeleton2 className="bg-gray-800" />
-      </div>
-    );
-  },
-});
+// const CyclePurchase = dynamic(() => import("./(currentCycle)/CyclePurchase"), {
+//   loading() {
+//     return (
+//       <div>
+//         <Skeleton2 className="bg-gray-800" />
+//       </div>
+//     );
+//   },
+// });
 
-const ChartPurchase = dynamic(() => import("./(recentPurchase)/ChartPurchase"));
+// const ChartPurchase = dynamic(() => import("./(recentPurchase)/ChartPurchase"));
 
 const PurchaseTable = dynamic(
   () => import("./(recentPurchase)/PurchaseTable"),
@@ -65,19 +65,19 @@ const page = async () => {
         </div>
 
         <div className=" mt-3 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-x-5">
-          <Reward reward={customer.reward} />
+          {/* <Reward reward={customer.reward} /> */}
 
-          <CyclePurchase />
+          {/* <CyclePurchase /> */}
 
-          <div className="col-span-2 mt-2 md:col-span-1  md:mt-0">
+          {/* <div className="col-span-2 mt-2 md:col-span-1  md:mt-0">
             <Levels />
-          </div>
+          </div> */}
         </div>
-        <>
+        {/* <>
           <ChartPurchase />
 
           <PurchaseTable />
-        </>
+        </> */}
       </div>
     </div>
   );
