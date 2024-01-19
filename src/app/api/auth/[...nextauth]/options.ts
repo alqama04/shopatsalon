b99a915845428a07ed602aa90abbdc922204d1fa
alqama04 <alqamaazmi11@gmail.com>
2024-01-19 22:27:57 +0530

    fix auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -38,13 +38,6 @@ export const options: NextAuthOptions = {
 
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            authorization: {
-                params: {
-                    prompt: 'consent',
-                    access_type: 'offline',
-                    response_type: 'code'
-                }
-            }
         })
     ],
 
