acc61986dbe411f82c2cf5c3d63f3ec58932dd43
alqama04 <alqamaazmi11@gmail.com>
2024-02-06 20:58:55 +0530

    fix grid layout

--- src/app/admin-dashboard/orders/[id]/Order.tsx
+++ src/app/admin-dashboard/orders/[id]/Order.tsx
@@ -51,7 +51,7 @@ const Order = ({ order }: orderProp) => {
         <p className="bg-gray-800 textarea text-white">
           Order - {order?.orderList}
         </p>
-        <div className="grid gap-2 mt-3 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid gap-2 mt-3 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
           {order.files.length
             ? order.files.map((file: string) => (
                 <div key={file}>
